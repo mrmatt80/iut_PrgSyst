990eaf6a47778725625c5ffc034100eec383f453
mrmatt80 <matt.8080@hotmail.fr>
2016-03-03 11:39:29 +0100

    TP5 finis

--- tp5/my_cat.c
+++ tp5/my_cat.c
@@ -3,26 +3,43 @@
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
+#include<string.h>
 
 int main(int argc,char* argv[]){
 	int desc;
-	char* c;
-	c=(char*)malloc(sizeof(char)*512)
-	if(argv[0]==""){perror("saisir un argument valide");
+	int i,d,j,taille;
+	char* c=NULL;
+	char* nomFi=NULL;
+	c=(char*)malloc(sizeof(char)*512);
+
+if(argc<2){
+	perror("saisir au moins un argument");
 	exit(1);}
 
-	if((desc=open(argv[1],O_RDONLY))){
-	perror("my_cat:open");
-	exit(2);}
-	
-	if(read(desc,&c,512)==-1){
-	perror("my_cat:read");
-	exit(3);}
-	
-	if
+for(j=1; j <argc;j++){
+
+	if((desc=open(argv[j],O_RDONLY))==-1){
+		perror("my_cat:open");
+		exit(2);}
+		i=0;
+		taille=sizeof(strlen(argv[j]))+25;
+		nomFi=(char*)malloc(taille);
+		strcat(nomFi,"---- Fichier");
+		strcat(nomFi,argv[j]);
+		strcat(nomFi," ----\n");
+		write(1,nomFi,taille);
+
+	while((d=read(desc,c,512))!=0){
+		write(1,c,d);
+		i++;
+		};
+
+	free(nomFi);
+}
 	
 	close(desc);
-	exit(0)
+	free(c);
+	exit(0);
 }
 	
 
